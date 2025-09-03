5f25c28bad12bd8aef2b42e2f7662f8acccf51d6
Thiago Getner Alves <103291791+ThiagoGetner@users.noreply.github.com>
2025-09-03 08:41:03 -0300

    data.js

---
+++ data.js
@@ -0,0 +1,4 @@
+// Dados fictícios para vendas e estoque
+const salesData = [120, 200, 150, 300, 250, 400, 350];
+const stockData = [80, 60, 90, 70, 100, 50, 65];
+const labels = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul"];
